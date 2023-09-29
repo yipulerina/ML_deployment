9a424f188fa7f4ffd736af17feb1b7be50a0c686
Yipu Lerina <yipulerina29@gmail.com>
2023-09-29 20:25:32 +0530

    updated data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ from src.logger import logging
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+
 
 @dataclass
 class DataIngestionConfig:
@@ -50,5 +52,11 @@ class DataIngestion:
         
         
 
+if __name__=="__main__":
+    obj = DataIngestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+    
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
         
     
