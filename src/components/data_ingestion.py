377a0b69f1adcd3206f18314c193ad8a199e35f7
Yipu Lerina <yipulerina29@gmail.com>
2023-09-29 13:37:26 +0530

    model training

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,9 @@
+import os 
+import pandas as pd
+import sys
+from src.exception import CustomException
+from src.logger import logging
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
