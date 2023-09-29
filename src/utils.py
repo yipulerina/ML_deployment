69a53dca763516133fe719d72c5ca08259ecbf4f
Yipu Lerina <yipulerina29@gmail.com>
2023-09-29 21:53:15 +0530

    updated model trainer

--- src/utils.py
+++ src/utils.py
@@ -19,3 +19,30 @@ def save_object(file_path, obj):
         
     except Exception as e:
         raise CustomException(e,sys)
+    
+    
+def evaluate_models(X_train,y_train,X_test,y_test,models,params):
+    try:
+        report = {}
+        
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para = params[list(models.keys())[i]]
+            
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+            
+            y_train_pred= gs.predict(X_train)
+            
+            y_test_pred = gs.predict(X_test)
+            
+            train_model_score = r2_score(y_train,y_train_pred)
+            
+            test_model_score = r2_score(y_test,y_test_pred)
+            
+            report[list(models.keys())[i]] =test_model_score
+            
+            return report
+                
+    except Exception as e:
+        raise CustomException(e,sys)
